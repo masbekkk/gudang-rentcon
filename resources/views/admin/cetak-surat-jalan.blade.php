4c7103c7c7cc867cb4bbc4c14eb71452abda8107
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2023-11-25 09:26:19 +0700

    new layout surat jalan

--- resources/views/admin/cetak-surat-jalan.blade.php
+++ resources/views/admin/cetak-surat-jalan.blade.php
@@ -1,208 +1,148 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Surat Jalan</title>
+
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" crossorigin="anonymous" />
+
     <style>
-        body {
+        * {
             font-family: Arial, sans-serif;
-        }
-
-        .container {
-            max-width: 800px;
-            margin: 0 auto;
-            padding: 20px;
-        }
-
-        .header {
-            text-align: center;
-            margin-bottom: 20px;
-        }
-
-        .logo {
-            max-width: 150px;
-            height: auto;
-        }
-
-        .address {
-            text-align: right;
-            margin-bottom: 20px;
-        }
-
-        .details {
-            margin-bottom: 20px;
-        }
-
-        .recipient {
-            margin-top: 20px;
-        }
-
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        /* @media print { */
-        th {
-            background-color: blue !important;
-            color: white;
-        }
-
-        /* } */
-
-        @media print {
-            #btn-print {
-                display: none;
-                visibility: hidden;
-            }
         }
 
         table,
         th,
         td {
-            border: 1px solid #000;
-        }
-
-        th,
-        td {
-            padding: 10px;
-            text-align: left;
-        }
-
-        .signatures {
-            display: flex;
-            /* Use Flexbox to make the children align horizontally */
-            justify-content: space-between;
-            /* Distribute space between the children */
-            max-width: 210mm;
-            /* Set the maximum width for A4 size */
-            margin: 0 auto;
-            /* Center the component horizontally on the page */
-        }
-
-        .signature {
-            text-align: center;
-            /* Center the content within each signature block */
-            width: calc(33.33% - 10px);
-            /* Divide the available space equally among 3 blocks with some margin */
-            margin: 0 5px;
-            /* Add some horizontal margin between signature blocks */
-        }
-
-        /* You can adjust the width and margin values to fit your specific design. */
-
-
-        .signature img {
-            max-width: 100px;
-            height: auto;
-        }
-
-        /* Signature Container Styles */
-        .table-title {
-            width: 100%;
+            border: 2px solid black;
             border-collapse: collapse;
         }
 
-        .nama {
-            width: 33.33%;
-            padding: 10px;
-            text-align: center;
-            vertical-align: top;
-            font-size: 16px;
-            font-family: Arial, sans-serif;
+        .ttd-box {
+            height: 10.5rem;
         }
 
-        /* Signature Text Styles */
-        .tgl {
-            font-size: 14px;
-            font-weight: bold;
+        .container-print {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
         }
 
-        .nama {
-            font-size: 16px;
+        .fs-small {
+            font-size: .9rem;
+            line-height: 1rem;
+            font-weight: 500;
         }
-
-        /* Adjust as needed for each specific case */
     </style>
-    <link rel="stylesheet" href="https://printjs-4de6.kxcdn.com/print.min.css" type="text/css">
-    <script type="text/javascript" src="https://printjs-4de6.kxcdn.com/print.min.js"></script>
 </head>
 
-<body>
-    <div class="container">
-        <div class="header">
-            <img src="{{ asset('logo_rentcon.jpeg') }}" alt="Logo Perusahaan" class="logo">
-            <h1>Surat Jalan</h1>
-            <button id="btn-print" style="display: block" onclick="window.print()">Cetak</button>
+<body class="min-vh-100 min-vw-100">
+    <div class="container-print">
+        <img style="width: 12rem; height: 12rem;" src="{{ asset('logo_rentcon.jpeg') }}" alt="RENTCON ICON">
+        <div class="mt-4 mb-3">
+            <h6 class="fw-bold mb-4">
+                RENTCON
+            </h6>
+            <p class="mb-0 fs-small">Ruko Gempol Interchange No.14 Ds. Legok Kec. Gempol</p>
+            <p class="mb-0 fs-small">Kab. Pasuruan, Jawa Timur</p>
+            <p class="mb-0 fs-small">Telp: 082330390941</p>
+            <p class="mb-0 fs-small">Email: rentcon.jatim@gmail.com</p>
         </div>
-
-        <div class="address">
-            <b>
-                <p>RENTCON</p>
-            </b>
-            <p>Jl. Raya Gondanglegi, Cangkringmalang</p>
-            <p>Kec. Beji Kab. Pasuruan,67154</p>
-            <p>Telp: 0823-3039-0941</p>
-        </div>
-
-        <div class="details">
-            <p>Tanggal: {{ date('d/m/Y', strtotime($data->tanggal)) }}</p>
-            <p>No. Surat: {{ $data->no_referensi }}</p>
-        </div>
-
-        <div class="recipient">
-            <h2>Penerima:</h2>
-            <b>
-                <p>{{ $data->penerima }} </p>
-            </b>
-            <p>{{ $data->alamat_penerima }},</p>
-            <p>{{ $data->kab_prov_penerima }}</p>
-            <p>Up: {{ $data->pic_penerima }}</p>
-        </div>
-
-        <table>
+        <h3 class="fw-bold text-end mb-0">SURAT JALAN</h3>
+        <table class="mb-4" style="width:100%">
             <tr>
-                <th>Produk</th>
-                <th>Deskripsi</th>
-                <th>Kuantitas</th>
+                <th class="py-3 px-3 w-50">
+                    <p class="mb-3 fs-small">Kepada</p>
+                    <h5 class="fw-bold" style="margin-bottom: .3rem;">{{ $data->penerima }}</h5>
+                    <p class="mb-0 fs-small">{{ $data->alamat_penerima }}</p>
+                    <p class="mb-0 fs-small">{{ $data->kab_prov_penerima }}</p>
+                    <p class="mb-0 fs-small">Up: {{ $data->pic_penerima }}</p>
+                </th>
+                <th class="pt-3 px-4 w-50 align-items-start" style="vertical-align: top;">
+                    <p class="border-bottom border-2 border-dark mb-2 pb-1 fs-small">
+                        Referensi : {{ $data->no_referensi }}
+                    </p>
+                    <p class="border-bottom border-2 border-dark mb-2 pb-1 fs-small">
+                        Tanggal: {{ date('d/m/Y', strtotime($data->tanggal)) }}
+                    </p>
+                </th>
+            </tr>
+        </table>
 
+        <table class="mb-4" style="width:100%">
+            <tr>
+                <th class="p-2 fs-small fw-bold">
+                    Produk
+                </th>
+                <th class="p-2 fs-small fw-bold" style="width: 29%;">
+                    Deskripsi
+                </th>
+                <th class="p-2 text-center fs-small fw-bold" style="width: 11%;">
+                    Kuantitas
+                </th>
             </tr>
             @foreach ($barangSuratJalan as $dataBSJ)
-                <tr>
-                    <td>{{ $dataBSJ->barang->nama_mesin }}</td>
-                    <td>{{ $dataBSJ->barang->spesifikasi_mesin }}</td>
-                    <td>{{ $dataBSJ->jumlah_barang }} pcs</td>
-                </tr>
+            <tr>
+                <th class="p-2 fs-small">
+                    {{ $dataBSJ->barang->nama_mesin }}
+                </th>
+                <th class="p-2 fs-small fw-bold">{{ $dataBSJ->barang->spesifikasi_mesin }}</th>
+                <th class="p-2 fs-small text-center">
+                    {{ $dataBSJ->jumlah_barang }} pcs
+                </th>
+            </tr>
             @endforeach
-
-
-
         </table>
-        <br>
-        <div class="signatures">
-            <div class="signature">
-                <p>Disiapkan</p>
-                {{-- <img src="signature1.png" alt="Tanda Tangan 1"> --}}
-                <p>{{ $data->disiapkan }}</p>
+
+        <table class="mb-4" style="width:100%">
+            <tr>
+                <th class="p-2 fs-small fw-bold" style="width: 20%;">
+                    Pesan
+                </th>
+                <th class="p-2 fs-small">
+                    Transfer via BCA 411-081-9361 A.N Muhammad Budi Hendrawan Sutopo
+                </th>
+            </tr>
+        </table>
+
+
+        <div class="row gx-5 mt-5">
+            <div class="col-3 ttd-box d-flex flex-column justify-content-between">
+                <p class="mb-0 fs-small text-center">
+                    Disiapkan Oleh
+                </p>
+                <p class="mb-0 fs-small text-center">
+                    ({{ $data->disiapkan }})
+                </p>
             </div>
-            <div class="signature">
-                <p>Dikirim</p>
-                {{-- <img src="signature2.png" alt="Tanda Tangan 2"> --}}
-                <p>{{ $data->dikirim }}</p>
+            <div class="col-3 ttd-box d-flex flex-column justify-content-between">
+                <p class="mb-0 fs-small text-center">
+                    Dikirim Oleh
+                </p>
+                <p class="mb-0 fs-small text-center">
+                    ({{ $data->dikirim }})
+                </p>
             </div>
-            <div class="signature">
-                <p>Diterima</p>
-                {{-- <img src="signature3.png" alt="Tanda Tangan 3"> --}}
-                <p>{{ $data->diterima }}</p>
+            <div class="col-3 ttd-box d-flex flex-column justify-content-between">
+                <p class="mb-0 fs-small text-center">
+                    Diterima Oleh
+                </p>
+                <p class="mb-0 fs-small text-center">
+                    ({{ $data->diterima }})
+                </p>
+            </div>
+            <div class="col-3 ttd-box d-flex flex-column justify-content-between">
+                <p class="mb-0 fs-small text-center">
+                    Dengan Hormat,
+                </p>
+                <p class="mb-0 fs-small text-center">
+                    Finance Dept
+                </p>
             </div>
         </div>
     </div>
-    <script>
-        document.addEventListener("DOMContentLoaded", function(event) {
-            window.print();
-        });
-    </script>
 </body>
 
 </html>
